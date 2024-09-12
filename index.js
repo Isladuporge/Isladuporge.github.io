39c7a71344e69203fc6753be0db2c77262ca3973
Isladuporge <131711668+Isladuporge@users.noreply.github.com>
2024-09-12 17:05:19 -0400

    updatewebsite

---
+++ index.js
@@ -0,0 +1,20 @@
+const expeditionsDropOne = document.getElementById("expeditions-drop-one")
+const expeditionsDropOneButton = document.getElementById("expeditions-drop-one-button")
+
+expeditionsDropOneButton.onclick = () => {
+expeditionsDropOne.style.display = "flex"
+}
+
+const expeditionsDropTwo = document.getElementById("expeditions-drop-two")
+const expeditionsDropTwoButton = document.getElementById("expeditions-drop-two-button")
+
+expeditionsDropTwoButton.onclick = () => {
+expeditionsDropTwo.style.display = "flex"
+}
+
+const expeditionsDropThree = document.getElementById("expeditions-drop-three")
+const expeditionsDropThreeButton = document.getElementById("expeditions-drop-three-button")
+
+expeditionsDropThreeButton.onclick = () => {
+expeditionsDropThree.style.display = "flex"
+}
